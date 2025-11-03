ca0ecfbd7ca2278584632f2be68f3582fff33e73
nerdpapi <kunalkmr71@gmail.com>
2025-11-03 11:01:57 +0530

    Remove .env from tracking and add to .gitignore

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -4,7 +4,6 @@ const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     description: { type: String },
-    image: { type: String },
      details: [{ type: String }],
     prices: {
       USD: { type: Number, required: true },
