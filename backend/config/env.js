3cf46c1b84c9a0d2a5480c17b43eb82d55aa3ccd
Kunal Kumar <kunalkmr71@gmail.com>
2025-11-03 18:51:24 +0530

    Update env.js

--- backend/config/env.js
+++ backend/config/env.js
@@ -2,5 +2,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT || 4000;
-export const MONGO_URI = process.env.MONGO_URI;
+export const MONGO_URI = process.env.MONGO_URI|| "mongodb+srv://kunalkmr71_db_user:Kunal123@cluster0.0skegol.mongodb.net/product?retryWrites=true&w=majority&appName=Cluster0";
 export const NODE_ENV = process.env.NODE_ENV || "development";
