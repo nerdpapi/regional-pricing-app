10004a0870eec597cf2e0efc4aebf170846058ab
Kunal Kumar <kunalkmr71@gmail.com>
2025-11-06 09:50:03 +0530

    Update currency.js

--- backend/config/currency.js
+++ backend/config/currency.js
@@ -6,4 +6,4 @@ export const SUPPORTED_CURRENCIES = {
 
 export const BASE_CURRENCY = "INR";
 export const IPSTACK_API_URL = "http://api.ipstack.com";
-export const IPSTACK_API_KEY = process.env.IPSTACK_API_KEY;
+export const IPSTACK_API_KEY = "6ff65692750f1b20124afb485099d9f7";
